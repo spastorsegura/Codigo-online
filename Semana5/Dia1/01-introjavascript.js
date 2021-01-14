5a95535c229c2d30c5364f325c9de1b7b13b6751
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2021-01-13 20:21:42 -0500

    avance de la clase5

---
+++ Semana5/Dia1/01-introjavascript.js
@@ -0,0 +1,30 @@
+// Hola soy un comentario :)
+console.log("Hola código!");
+
+//mis variables
+//siempre deben comenzar con una letra o  y son sensibles a las mayúsculas y minúsculas
+var miNombre = "Osmar"
+
+var edad = 30;
+
+console.log(miNombre);
+console.log(edad);
+
+
+// Reasignando el valor de mi variable
+edad=25;
+
+console.log(edad);
+
+
+//Operaciones con variables
+
+edad= edad+10
+
+console.log(edad);
+
+//Orden de los operadores
+
+edad = edad*2+10;
+
+console.log(edad);
---
+++ Semana5/Dia1/04-tiposdedatos.js
@@ -0,0 +1,35 @@
+// Tipo de dato - String - cadena de texto
+var nombre="Luna";
+var edad=2;
+
+// Tipo de dato, númerico
+var edad=2;
+var promedio=13.5;
+
+// Booleanos
+var aprobado=true;
+var esDeDia=false;
+
+console.log(nombre);
+console.log(edad);
+console.log(aprobado);
+
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof aprobado);
+
+// Cambio de tipo de dato
+var versatil="positivo";
+versatil=500;
+versatil=true;
+
+console.log(versatil);
+
+
+//tipos de datos raros
+var x;
+console.log(x);
+
+var y=null;
+console.log(null);
+
