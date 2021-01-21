43cbe405ccb7ad001855ab1d567dd833324f7547
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2021-01-20 23:28:43 -0500

    subir avance

---
+++ Semana6/Dia1/04-eventos.js
@@ -0,0 +1,52 @@
+// Escuchando eventos
+
+var miBoton=document.getElementById("boton");
+
+//Este addEventListener("Evento",función)
+miBoton.addEventListener("click",function(event){
+    console.log(event);
+    alert("Haz dado clic!");
+})
+
+var miLink = document.getElementById("link");
+
+// miLink.setAttribute("target","_blank")
+
+miLink.addEventListener("click",function(evento){
+    console.log(evento);
+    evento.preventDefault();
+    console.log("Click en el link");
+})
+
+//-------------------------
+
+var inputtexto=document.getElementById("inputtexto");
+var btntexto=document.getElementById("botoninput");
+var divLista=document.getElementById("lista");
+
+btntexto.addEventListener("click", function(){
+    console.log(inputtexto.value);
+})
+//keydown(cuando se apreta),keypress(se mantiene),keyup(apretas y sueltas)
+
+//
+/* inputtexto.addEventListener("keyup",function(e){
+    console.log(e)
+    if(e.key==="Enter"){
+        console.log(inputtexto.value)
+        
+    }
+    // console.log(e)
+})
+ */
+
+inputtexto.addEventListener("keyup",function(e){
+    if(e.key==="Enter"){
+        console.log("Enter!!")
+        console.log(e)
+        var nuevoParrafo = document.createElement("p");
+        nuevoParrafo.innerHTML=inputtexto.value;
+        divLista.appendChild(nuevoParrafo);
+    }
+    
+})
