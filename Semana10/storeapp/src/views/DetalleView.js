48aa09756c3cd624073b39302fe1ecb4d45e4698
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2021-02-18 20:13:40 -0500

    subir

--- Semana10/storeapp/src/views/DetalleView.js
+++ Semana10/storeapp/src/views/DetalleView.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, {useState,useEffect,Fragment} from 'react'
+import {obtenerProductoPorId} from "../services/ProductoService"
 
 export default function DetalleView(props) {
-    console.log(props)
+    // console.log(props.match.params.id)
+    const productoID = props.match.params.id
+
+const [miProducto,setMiProducto]=useState({})
+
+const getProduct=async()=>{
+    let productoObtenido = await obtenerProductoPorId
+    setMiProducto(productoObtenido)
+}
+
+useEffect(()=>{
+    getProduct()
+},[])
+
     return (
-        <div>
-            Hola yo soy la vista detalle
-        </div>
+        
     )
 }
