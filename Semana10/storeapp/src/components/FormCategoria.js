b095bda3add2911e27232d79368631b4e8d0657f
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2021-02-20 08:51:00 -0500

    subir

--- Semana10/storeapp/src/components/FormCategoria.js
+++ Semana10/storeapp/src/components/FormCategoria.js
@@ -1,41 +1,42 @@
-import React, {useState} from "react";
-import {crearCategoria} from '../services/categoriaService'
+import React,{useState} from 'react'
+import {crearCategoria} from "../services/categoriaService"
 
 export default function FormCategoria() {
-    const [nombreCategoria,setNombreCategoria]=useState("")
-
-    const manejarSubmit = async (ev)=>{
-        //prevenimos que se ejecute el evento por defecto submit
-        ev.preventDefault()
-
-        //crea objeto como pide el backend
-        let objCategoria = {
-            nombre:nombreCategoria
-        }
-        //hago petición
-        let response = await crearCategoria(objCategoria)
-        console.log(response)
+  const [nombreCategoria, setNombreCategoria] = useState("")
+  //esta función va a recibir el evento
+  const manejarSubmit = async (ev) => {
+    //prevenimos que se ejecute el evento predeterminado del submit
+    ev.preventDefault()
+    //creamnos el objeto tal como le pide el backend
+    let objCategoria = {
+      nombre:nombreCategoria
     }
+    //hago la petición
+    let response = await crearCategoria(objCategoria)
+    console.log(response)
+  }
 
   return (
     <div>
-      <form onSubmit={(ev)=>{manejarSubmit(ev)}}>
+      <form onSubmit={(ev) => manejarSubmit(ev)}>
         <div className="mb-2">
-          <label className="form-label">Nombre de la categoría</label>
+          <label className="form-label">
+            Nombre de la Categoria
+            </label>
           <input 
-          className="form-control" 
-          type="text"
-          value={nombreCategoria}
-          onChange={(e)=>{setNombreCategoria(e.target.value)}}
+            className="form-control" 
+            type="text"
+            value={nombreCategoria}
+            onChange={(e) => {setNombreCategoria(e.target.value)}}
           />
           <small className="form-text">
-              Ej.:Ofertas o regalos
+            Ej.: Ofertas ó Regalos
           </small>
-          
         </div>
-        <button type="submit" className="btn btn-primary">Crear categoría</button>
-        
+        <button type="submit" className="btn btn-primary">
+        Crear Categoria
+        </button>
       </form>
     </div>
-  );
+  )
 }
--- Semana10/storeapp/src/views/CrearCategoriaView.js
+++ Semana10/storeapp/src/views/CrearCategoriaView.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import FormCategoria from '../components/FormCategoria'
+import FormCategoria from "../components/FormCategoria"
+
 
 export default function CrearCategoriaView() {
-    return (
-        <div>
-            <h1 className="display-6">Crear Categoria</h1>
-            <FormCategoria/>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="display-6">Crear Categoria</h1>
+      <FormCategoria/>
+    </div>
+  )
 }
