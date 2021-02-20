d40a0b72a3099fa74c855741bd9bcf6d84483485
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2021-02-19 20:09:30 -0500

    subir

--- Semana10/storeapp/src/components/FormCategoria.js
+++ Semana10/storeapp/src/components/FormCategoria.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function FormCategoria() {
+    const [nombreCategoria,setNombreCategoria]=useState("")
+
   return (
     <div>
       <form>
         <div className="mb-2">
           <label className="form-label">Nombre de la categoría</label>
-          <input className="form-control" type="text"/>
+          <input 
+          lassName="form-control" 
+          type="text"
+          value={nombreCategoria}
+          onChange={(e)=>{setNombreCategoria(e.target.value)}}
+          />
           <small className="form-text">
               Ej.:Ofertas o regalos
           </small>
